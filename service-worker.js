7f8a9c3cc703d9b8bd10c1784a57e304e76f2118
Peterosuma01 <122818053+Peterosuma01@users.noreply.github.com>
2026-01-23 11:12:48 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'swal-payroll-v1';
+const urlsToCache = [
+  '/swal-payroll-calculator/',
+  '/swal-payroll-calculator/index.html',
+  '/swal-payroll-calculator/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
